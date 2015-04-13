8887ac8a011824828a1e9f064baa08ac2ef7198b
Marlon Yukim <kimskiper@users.noreply.github.com>
2015-04-13 17:57:26 +0800

    commented out nginx teamcity config

--- spiral/recipes/teamcity.rb
+++ spiral/recipes/teamcity.rb
@@ -11,17 +11,18 @@ link log_folder do
   not_if { ::File.exists?(log_folder) }
 end
 
-template '/etc/nginx/sites-enabled/teamcity' do
-  source 'nginx_proxy_pass.erb'
-  verify "nginx -t -c %{path}"
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0755'
-  notifies :restart, "service[nginx]"
-end
 
-file '/etc/nginx/sites-enabled/default' do
-  action :delete
-  notifies :restart, "service[nginx]"
-end
+#Commenting it out to test the LB
+#template '/etc/nginx/sites-enabled/teamcity' do
+##  verify "nginx -t -c %{path}"
+#  action :create
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#  notifies :restart, "service[nginx]"
+#end
+
+#file '/etc/nginx/sites-enabled/default' do
+#  action :delete
+#  notifies :restart, "service[nginx]"
+#end
