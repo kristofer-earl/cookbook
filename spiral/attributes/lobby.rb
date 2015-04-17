b53d0ce0d3973fba1508a115c7ecdf3c8d81b893
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-17 12:16:21 +0800

    add lobby service via supervisor

---
+++ spiral/attributes/lobby.rb
@@ -0,0 +1,1 @@
+default['spiral']['lobby']['java_opts'] = '-Xmx1024m -Xms512m -XX:MaxPermSize=256m'
---
+++ spiral/recipes/lobby.rb
@@ -0,0 +1,19 @@
+include_recipe 'spiral::supervisor'
+
+log_path = '/var/log/lobby'
+
+directory log_path do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create  
+end
+
+supervisor_service 'lobby' do
+  action :enable
+  autostart true
+  user 'root'
+  directory log_path
+  environment 'HOME' => log_path
+  command "java #{node['spiral']['lobby']['java_opts']} -jar /opt/lobby/lobby.jar" 
+end
