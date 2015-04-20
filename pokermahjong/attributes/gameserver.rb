97f274a34f62106191da157fbd6634de2ee5fe75
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-20 11:53:55 +0800

    add revision branch

--- pokermahjong/attributes/gameserver.rb
+++ pokermahjong/attributes/gameserver.rb
@@ -1,2 +1,2 @@
 default['gameserver']['git_repository'] = 'git@github.com:ProjectSoftwareDeveloper/ProductPMahjongServer.git'
-default['gameserver']['git_revision'] = 
+default['gameserver']['git_revision'] = 'master' 
