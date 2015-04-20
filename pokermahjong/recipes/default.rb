1e537b5f26538ff9a282cf7dc78b9da1786a8716
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-20 15:41:28 +0800

    use key for root instead

--- pokermahjong/recipes/default.rb
+++ pokermahjong/recipes/default.rb
@@ -1,7 +1,7 @@
-cookbook_file '/home/ubuntu/.ssh/id_rsa' do
+cookbook_file '/root/.ssh/id_rsa' do
   source 'swcore-git.key'
-  owner  'ubuntu'
-  group  'ubuntu'
+  owner  'root'
+  group  'root'
   mode   '0600'
   action :create
 end
