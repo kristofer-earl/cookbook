37cbd01ebb77184566668d11c4125a7ad18da23f
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-20 14:34:27 +0800

    add lobby recipe and merge to git attributes

--- pokermahjong/recipes/lobby.rb
+++ pokermahjong/recipes/lobby.rb
@@ -16,22 +16,22 @@ directory src_path do
   action :create
 end
 
-git "#{src_path}/gameserver" do
+git "#{src_path}/lobby" do
   repository node[:git][:repository]
   revision node[:git][:revision]
   action :sync
-  #notifies :run, "bash[compile_gameserver]"
+  #notifies :run, "bash[compile_lobby]"
 end
 
-template "#{src_path}/gameserver/GameServer/src/main/config/facebook_staging/game_server.properties" do
-  source 'facebook_staging/gameserver/game_server.properties.erb'
+template "#{src_path}/lobby/LobbyServer/src/main/config/facebook_staging/lobby_server.properties" do
+  source 'facebook_staging/lobby/lobby_server.properties.erb'
   owner  'root'
   group  'root'
   mode   '0755'
   action :create 
 end
 
-template "#{src_path}/gameserver/ServerCore/src/main/config/facebook_staging/application.properties" do
+template "#{src_path}/lobby/ServerCore/src/main/config/facebook_staging/application.properties" do
   source 'facebook_staging/servercore/application.properties.erb'
   owner  'root'
   group  'root'
@@ -40,30 +40,30 @@ template "#{src_path}/gameserver/ServerCore/src/main/config/facebook_staging/app
 end
 
 execute 'setup_server_core' do
-  cwd     "#{src_path}/gameserver/ServerCore"
+  cwd     "#{src_path}/lobby/ServerCore"
   command 'mvn -f pom_base.xml install'
 end
 
 execute 'compile_server_core' do
-  cwd     "#{src_path}/gameserver/ServerCore"
+  cwd     "#{src_path}/lobby/ServerCore"
   command 'mvn install -DskipTests=true -Pfacebook_staging'
 end
 
-execute 'compile_gameserver' do
-  cwd     "#{src_path}/gameserver/GameServer"
+execute 'compile_lobby' do
+  cwd     "#{src_path}/lobby/LobbyServer"
   command 'mvn package -Dmaven.test.skip=true -Pfacebook_staging'
 end
 
-directory '/opt/gs' do
+directory '/opt/lobby' do
   owner  'root'
   group  'root'
   mode   '0755'
   action :create
 end
 
-execute 'install_gameserver' do
-  cwd     "#{src_path}/gameserver/GameServer/target"
-  command 'install -m755 GameServer-1.0-SNAPSHOT-jar-with-dependencies.jar /opt/gs/gameserver.jar'
+execute 'install_lobby' do
+  cwd     "#{src_path}/lobby/LobbyServer/target"
+  command 'install -m755 LobbyServer-1.0-SNAPSHOT-jar-with-dependencies.jar /opt/gs/lobby.jar'
 end
 
 directory log_path do
@@ -81,11 +81,11 @@ template "#{log_path}/logback.properties" do
   mode   '0755'
 end
 
-supervisor_service 'gameserver' do
+supervisor_service 'lobby' do
   action :enable
   autostart true
   user 'root'
   directory log_path
   environment 'HOME' => log_path
-  command "java #{node['spiral']['gameserver']['java_opts']} -jar /opt/gs/gameserver.jar"
+  command "java #{node['spiral']['lobby']['java_opts']} -jar /opt/lobby/lobby.jar"
 end
