9af2cb51502df141da6cc29f2d0efa4af8e69f2c
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-20 11:32:20 +0800

    add pokermahjong cookbook for deployments

---
+++ pokermahjong/attributes/gameserver.rb
@@ -0,0 +1,2 @@
+default['gameserver']['git_repository'] = 'git@github.com:ProjectSoftwareDeveloper/ProductPMahjongServer.git'
+default['gameserver']['git_revision'] = 
---
+++ pokermahjong/recipes/gameserver.rb
@@ -0,0 +1,12 @@
+include_recipe 'spiral::default'
+
+package 'maven'
+package 'ant'
+
+git "/opt/deploy/gameserver" do
+  repository node[:gameserver][:git_repository]
+  revision node[:gameserver][:revision]
+  action :sync
+  enable_submodules :true
+  #notifies :run, "bash[compile_gameserver]"
+end
