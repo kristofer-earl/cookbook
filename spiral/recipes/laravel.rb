456a301ba0561dfae934de3a0959e4f0c548cb96
Alexander Baldeck <alexb@engagespark.com>
2015-04-21 19:07:39 +0800

    install composer and add wrapper script for hhvm

--- spiral/recipes/laravel.rb
+++ spiral/recipes/laravel.rb
@@ -2,6 +2,19 @@ include_recipe 'apt'
 include_recipe 'hhvm3'
 include_recipe 'spiral::nginx'
 
+remote_file '/usr/share/composer.phar' do
+  source 'https://getcomposer.org/composer.phar' 
+  not_if { ::File.exists?('/usr/share/composer.phar') }
+end
+
+cookbook_file '/usr/local/bin/composer' do
+  source 'composer.sh'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
 template '/etc/nginx/sites-available/laravel_nginx.conf' do
   source   'laravel_nginx.conf.erb'
   owner    'root'
