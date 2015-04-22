320f57c6c12e013542227274a088977084bcb387
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 18:08:53 +0800

    add wallet git repository

--- pokermahjong/attributes/git.rb
+++ pokermahjong/attributes/git.rb
@@ -3,3 +3,7 @@ default['git']['pokermahjong']['revision']   = 'master'
 
 default['git']['cas']['repository'] = 'git@github.com:ProjectSoftwareDeveloper/CentralSystemAuth.git'
 default['git']['cas']['revision']   = 'master'
+
+default['git']['wallet']['repository'] = 'git@github.com:ProjectSoftwareDeveloper/MainWallet.git'
+default['git']['wallet']['revision']   = 'master'
+
