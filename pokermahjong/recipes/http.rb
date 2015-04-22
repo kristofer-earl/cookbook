c306aac05123869fb6e179f3c95d5a5be8c98f17
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 11:05:54 +0800

    build HttpServer on instance as well

--- pokermahjong/recipes/http.rb
+++ pokermahjong/recipes/http.rb
@@ -3,6 +3,69 @@ include_recipe 'spiral::tomcat8'
 
 tomcat8_path = '/opt/tomcat8'
 
+git "#{src_path}/httpsrv" do
+  repository node[:git][:repository]
+  revision node[:git][:revision]
+  action :sync
+  #notifies :run, "bash[compile_httpsrv]"
+end
+
+template "#{src_path}/httpsrv/HttpServer/src/main/config/facebook_staging/application.properties" do
+  source 'facebook_staging/httpserver/application.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{src_path}/httpsrv/HttpServer/src/main/config/facebook_staging/wallet.properties" do
+  source 'facebook_staging/httpserver/wallet.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{src_path}/httpsrv/HttpServer/src/main/config/facebook_staging/payment.properties" do
+  source 'facebook_staging/httpserver/payment.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{src_path}/httpsrv/HttpServer/src/main/config/facebook_staging/logback.xml" do
+  source 'facebook_staging/httpserver/logback.xml'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+
+template "#{src_path}/httpsrv/ServerCore/src/main/config/facebook_staging/application.properties" do
+  source 'facebook_staging/servercore/application.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+execute 'setup_server_core' do
+  cwd     "#{src_path}/httpsrv/ServerCore"
+  command 'mvn -f pom_base.xml install'
+end
+
+execute 'compile_lobby' do
+  cwd     "#{src_path}/httpsrv/HttpServer"
+  command 'mvn package -Dmaven.test.skip=true -Pfacebook_staging'
+end
+
+execute 'install_lobby' do
+  cwd     "#{src_path}/httpsrv/HttpServer/target"
+  command "install -m755 HttpServer.war #{tomcat8_path}/webapps/HttpServer.war" 
+end
+
 cookbook_file 'crossdomain.xml' do
   path '/usr/share/nginx/html/crossdomain.xml'
   owner 'www-data'
