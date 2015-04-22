f487bffad3a1bcbacab557cb736ce39bfaab4dbe
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 12:56:38 +0800

    remove README.md usage for long_description

--- pokermahjong/metadata.rb
+++ pokermahjong/metadata.rb
@@ -3,7 +3,6 @@ maintainer        'Spiralworks PokerMahjong'
 maintainer_email  'devops@spiralwks.com'
 license           'Private'
 description       'Sets up PokerMahjong stack'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.0.1'
 
 depends 'spiral'
--- spiral/metadata.rb
+++ spiral/metadata.rb
@@ -3,7 +3,6 @@ maintainer        'Spiralworks Core'
 maintainer_email  'devops@spiralwks.com'
 license           'Private'
 description       'Configures basic infrastruture resused within the contexts of many inhouse applications'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.0.1'
 
 depends 'apt'
