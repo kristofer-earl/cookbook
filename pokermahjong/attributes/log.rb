4fe809f5cc05b8e4421ebd7cc97d5cb995534c18
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 19:47:56 +0800

    fix typo

--- pokermahjong/attributes/log.rb
+++ pokermahjong/attributes/log.rb
@@ -1,4 +1,4 @@
 default['pokermahjong']['http']['log_path']    = '/var/log/httpsrv'
 default['pokermahjong']['cas']['log_path']     = '/var/log/cas'
 default['pokermahjong']['wallet']['log_path']  = '/var/log/wallet'
-defialt['pokermahjong']['pmbo']['log_path']    = '/var/log/pmbo'
+default['pokermahjong']['pmbo']['log_path']    = '/var/log/pmbo'
