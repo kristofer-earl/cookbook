c0052cc1a874168e64ce3caaef07f4dc8a711b90
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 11:21:47 +0800

    notify tomcat8 to restart

--- pokermahjong/recipes/http.rb
+++ pokermahjong/recipes/http.rb
@@ -67,6 +67,7 @@ end
 execute 'install_lobby' do
   cwd     "#{src_path}/httpsrv/HttpServer/target"
   command "install -m755 HttpServer.war #{tomcat8_path}/webapps/HttpServer.war" 
+  notifies :restart, "service[tomcat8]"
 end
 
 cookbook_file 'crossdomain.xml' do
