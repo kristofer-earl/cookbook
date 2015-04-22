e0aed3288eee8029c18b469eece941a640782c27
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-22 17:05:48 +0800

    build CAS onsite

--- pokermahjong/recipes/cas.rb
+++ pokermahjong/recipes/cas.rb
@@ -5,10 +5,62 @@ include_recipe 'spiral::default'
 
 tomcat_path = '/opt/tomcat'
 
-git "#{node['pokermahjong']['src_path']}/httpsrv" do
+git "#{node['pokermahjong']['src_path']}/cas" do
   repository node[:git][:cas][:repository]
   revision node[:git][:cas][:revision]
   action :sync
+end
+
+template "#{node['pokermahjong']['src_path']}/cas/userbo/src/main/resources/cas.properties" do
+  source 'cas/cas.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{node['pokermahjong']['src_path']}/cas/userbo/src/main/resources/jdbc.properties" do
+  source 'cas/jdbc.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{node['pokermahjong']['src_path']}/cas/userbo/src/main/resources/log4j.properties" do
+  source 'cas/log4j.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+template "#{node['pokermahjong']['src_path']}/cas/userbo/src/main/resources/com/kvxz/memcached/memcached.xml" do
+  source 'cas/memcached.xml.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
+
+execute 'compile_casbo' do
+  cwd     "#{node['pokermahjong']['src_path']}/cas/userbo"
+  command 'mvn clean install -DskipTests=true'
+end
+
+execute 'install_casbo' do
+  cwd     "#{node['pokermahjong']['src_path']}/cas/userbo/target"
+  command "install -m755 userbo.war #{tomcat8_path}/webapps/userbo.war"
+end
+
+execute 'compile_casfo' do
+  cwd     "#{node['pokermahjong']['src_path']}/cas/userfo"
+  command 'mvn clean install -DskipTests=true'
+end
+
+execute 'install_casfo' do
+  cwd     "#{node['pokermahjong']['src_path']}/cas/userfo/target"
+  command "install -m755 userfo.war #{tomcat8_path}/webapps/userfo.war"
 end
 
 directory node['pokermahjong']['cas']['log_path'] do
@@ -16,4 +68,8 @@ directory node['pokermahjong']['cas']['log_path'] do
   group node['spiral']['users']['group']
   mode '0755'
   action :create  
+end
+
+supervisor_service 'tomcat' do
+  action :restart
 end
