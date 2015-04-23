2bf866c0e92ed1af7fcd0bf0ab8cc8bc705c214b
DevOps Marlon <marlon@DevOpss-MacBook-Pro.local>
2015-04-23 14:08:46 +0800

    removed default installed php and apache2 in the cookbook

--- prometheus-cms/recipes/default.rb
+++ prometheus-cms/recipes/default.rb
@@ -7,12 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "apache2"
-include_recipe "mysql::client"
-include_recipe "mysql::server"
-include_recipe "php"
-include_recipe "php::module_mysql"
-include_recipe "apache2::mod_php5"
+#include_recipe "php::module_mysql"
+#include_recipe "apache2::mod_php5"
 include_recipe "nginx"
 include_recipe "hhvm"
 include_recipe "composer"
