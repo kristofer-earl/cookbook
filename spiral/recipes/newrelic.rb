57498c834eeba69e1e056a678e7666d9424e40eb
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-23 20:40:56 +0800

    add newrelic repo

---
+++ spiral/recipes/newrelic.rb
@@ -0,0 +1,10 @@
+include_recipe 'apt'
+
+apt_repository 'newrelic' do
+  uri 'http://apt.newrelic.com/debian'
+  distribution 'newrelic' 
+  components ['non-free']
+  key 'https://download.newrelic.com/548C16BF.gpg'
+end
+
+
