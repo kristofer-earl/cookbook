ac0f54c1ef0761bd3a8129088e40ecd30e663191
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-04-23 18:14:26 +0800

    up Xmx limit to 2GB

--- spiral/attributes/gameserver.rb
+++ spiral/attributes/gameserver.rb
@@ -1,3 +1,3 @@
 default['spiral']['gameserver']['log_level']['pm'] = 'INFO'
 default['spiral']['gameserver']['log_level']['root'] = 'INFO'
-default['spiral']['gameserver']['java_opts'] = '-Xmx1024m -Xms512m -XX:MaxPermSize=256m'
+default['spiral']['gameserver']['java_opts'] = '-Xm2048m -Xms512m -XX:MaxPermSize=256m'
