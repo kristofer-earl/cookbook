a97cd8645cb94c8c357112192c6560ca4f4fbdcc
DevOps Marlon <marlon@DevOpss-MacBook-Pro.local>
2015-04-24 15:20:26 +0800

    test

--- prometheus-cms/recipes/cms.rb
+++ prometheus-cms/recipes/cms.rb
@@ -19,6 +19,23 @@ template '/opt/cms/test.php' do
 	group 'www-data'
 end
 
-execute "create_symlink" do
+execute "create_symlink_cmsdir" do
 	command 'ln -s /var/www/html/cms /opt/cms'
+	not_if { ::File.directory?("/var/www/html/cms")
+end
+
+template '/etc/nginx/sites-available/cms'
+	source 'cms.erb'
+	mode '644'
+	owner 'www-data'
+	group 'www-data'
+end
+
+execute "create_symlink_cmssite" do
+        command 'ln -s /etc/nginx/sites-enabled/cms /etc/nginx/sites-availbale/cms'
+	not_if { ::File.exists?("/etc/nginx/sites-enabled/cms")
+end
+
+service 'nginx'
+	action :restart
 end
