5e97087bbd909fbe881ceeb310912b003a41c4fe
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-06 18:20:06 +0800

    add rudimentary config details for use in Java stack

--- pokermahjong/recipes/default.rb
+++ pokermahjong/recipes/default.rb
@@ -9,3 +9,11 @@ directory node['pokermahjong']['src_path'] do
   mode '0755'
   action :create
 end
+
+template "/etc/serverinfo.properties" do
+  source 'serverinfo.properties.erb'
+  owner  'root'
+  group  'root'
+  mode   '0755'
+  action :create
+end
