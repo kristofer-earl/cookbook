a7f72c3e55e0949fc98c858762547b10743b2cdc
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-06 20:30:05 +0800

    configure serverinfo.properties' location

--- pokermahjong/attributes/gameserver.rb
+++ pokermahjong/attributes/gameserver.rb
@@ -1,3 +1,3 @@
 default['spiral']['gameserver']['log_level']['pm'] = 'INFO'
 default['spiral']['gameserver']['log_level']['root'] = 'INFO'
-default['spiral']['gameserver']['java_opts'] = '-javaagent:/opt/newrelic/newrelic.jar -Xmx2048m -Xms512m -XX:MaxPermSize=256m'
+default['spiral']['gameserver']['java_opts'] = '-javaagent:/opt/newrelic/newrelic.jar -Xmx2048m -Xms512m -XX:MaxPermSize=256m -Dpmj.serverInfo=/etc/serverinfo.properties'
