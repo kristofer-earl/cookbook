eb68bb2764430b0fbb78e9ced9b4e738d371210c
Marlon Yukim <kimskiper@devs-MacBook-Pro.local>
2015-05-06 12:16:39 +0800

    added ruby package in capistrano cookbook for compliance

--- capistrano/metadata.rb
+++ capistrano/metadata.rb
@@ -1,2 +1,8 @@
-#
+name             'capistrano'
+maintainer       'SpiralWorks'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures capistrano'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
 
--- capistrano/recipe/install.rb
+++ capistrano/recipe/install.rb
@@ -1,4 +1,8 @@
 #
+package 'ruby' do
+        action :install
+end
+
 execute 'capistrano_install' do
-	command 'gem install capistrano'
+        command 'gem install capistrano'
 end
