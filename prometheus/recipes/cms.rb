acaa2932cc05393923a52275dcdfac3c4dc8b806
Marlon Yukim <kimskiper@devs-MacBook-Pro.local>
2015-05-06 13:51:05 +0800

    fixed attribute fetching

--- prometheus/recipes/cms.rb
+++ prometheus/recipes/cms.rb
@@ -15,11 +15,37 @@ package 'memcached' do
         action :install
 end
 
+directory '/var/www' do
+        owner 'www-data'
+        group 'www-data'
+        mode '0755'
+        action :create
+end
+
 directory '/var/www/cms' do
         owner 'www-data'
         group 'www-data'
         mode '0755'
         action :create
+end
+
+directory '/tmp/cms' do
+        mode '0755'
+        action :create
+end
+
+template '/tmp/cms/settings.php' do
+        source 'settings.php.erb'
+        mode '644'
+        owner 'www-data'
+        group 'www-data'
+        variables ({ :database => node['prometheus']['cms_db_name'],
+                :username => node['prometheus']['cms_db_user'],
+                :password => node['prometheus']['cms_db_pass'],
+                :host => node['prometheus']['cms_db_host'],
+                :port => node['prometheus']['cms_db_port'],
+                :driver => node['prometheus']['cms_db_driver'],
+                :prefix => node['prometheus']['cms_db_prefix'] })
 end
 
 directory '/var/log/nginx/cms' do
@@ -29,24 +55,24 @@ directory '/var/log/nginx/cms' do
         action :create
 end
 
-template '/opt/cms/test.php' do
-	source 'test.php.erb'
-	mode '755'
-	owner 'www-data'
-	group 'www-data'
+template '/var/www/cms/test.php' do
+        source 'test.php.erb'
+        mode '755'
+        owner 'www-data'
+        group 'www-data'
 end
 
 template '/etc/nginx/sites-available/cms' do
-	source 'cms.erb'
-	mode '755'
-	owner 'www-data'
-	group 'www-data'
+        source 'cms.erb'
+        mode '755'
+        owner 'www-data'
+        group 'www-data'
 end
 
 link '/etc/nginx/sites-enabled/cms' do
-	to '/etc/nginx/sites-available/cms'
-	link_type :symbolic
-	action :create
+        to '/etc/nginx/sites-available/cms'
+        link_type :symbolic
+        action :create
 end
 
 link '/etc/nginx/sites-enabled/default' do
@@ -54,5 +80,5 @@ link '/etc/nginx/sites-enabled/default' do
 end
 
 service 'nginx' do
-	action :restart
+        action :restart
 end
