1d2a3aef40956fc6d2173bb07935cf3c220325a3
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-08 11:58:31 +0800

    fix non OpsWorks setups

--- spiral/attributes/default.rb
+++ spiral/attributes/default.rb
@@ -1,1 +1,3 @@
-default['pokermahjong']['my_layer'] = node['opsworks']['instance']['layers'].first
+if defined? node['opsworks']['instance']['layers'].first
+  default['pokermahjong']['my_layer'] = node['opsworks']['instance']['layers'].first
+end
