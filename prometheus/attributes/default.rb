1464f36d1d7c45a3500dfdcc0737ebc96dfc4818
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-08 17:10:47 +0800

    take our memcached from api and create dedicated install with custom config

--- prometheus/attributes/default.rb
+++ prometheus/attributes/default.rb
@@ -5,9 +5,3 @@ default["prometheus"]["cms_db_user"] = "\'root\'"
 default["prometheus"]["cms_db_pass"] = "\'\'"
 default["prometheus"]["cms_db_driver"] = "\'mysql\'"
 default["prometheus"]["cms_db_prefix"] = "\'\'"
-
-
-default[:memcached][:memory] = 512
-default[:memcached][:port] = 11211
-default[:memcached][:user] = 'nobody'
-default[:memcached][:max_connections] = "4096"
