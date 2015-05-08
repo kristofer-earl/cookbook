fc82e7c77373845cbb573a52ef7175e28172a4ec
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-08 15:32:20 +0800

    remove if exists

--- spiral/recipes/phpfpm.rb
+++ spiral/recipes/phpfpm.rb
@@ -21,12 +21,10 @@ end
 
 link '/etc/php5/mods-available/mcrypt.ini' do
   to '/etc/php5/cli/conf.d/99-mcrypt.ini'
-  not_if { ::File.exists?(tomcat_path) }
 end
 
 link '/etc/php5/mods-available/mcrypt.ini' do
   to '/etc/php5/fpm/conf.d/99-mcrypt.ini'
-  not_if { ::File.exists?(tomcat_path) }
 end
 
 bash "debconf_newrelic" do
