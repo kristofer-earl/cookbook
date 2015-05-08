9be3244faa610b66b4cd293a27c5eba2877408e1
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-08 11:47:27 +0800

    hack to enable sudo for deploy user so we have something before enabling proper sudoers handling

--- prometheus/recipes/default.rb
+++ prometheus/recipes/default.rb
@@ -15,3 +15,13 @@ directory '/home/deploy/.ssh' do
   mode '0700'
   action :create
 end
+
+# hack sudoers
+
+bash "insert_sudo_for_deploy" do
+  user "root"
+  code <<-EOS
+  echo "deploy ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+  EOS
+  not_if "grep -q 'deploy ALL=(ALL) NOPASSWD:ALL' /etc/sudoers"
+end
