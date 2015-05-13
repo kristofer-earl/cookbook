818f23672ee41be13a92ef79c9e6ed8e45e26df0
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-13 19:25:25 +0800

    fixing a sensual array

--- spiral/recipes/nginx.rb
+++ spiral/recipes/nginx.rb
@@ -1,7 +1,7 @@
 apt_repository 'nginx' do
   uri 'http://ppa.launchpad.net/nginx/stable/ubuntu'
   distribution node['lsb']['codename'] 
-  components 'main'
+  components ['main']
   keyserver 'keyserver.ubuntu.com'
   key 'C300EE8C'
   action :add
