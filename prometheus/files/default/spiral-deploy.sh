120f2d27123de3f9bff221792d2b49814fe8b8b3
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-14 10:46:13 +0800

    add composer vendor/bin to PATH

--- prometheus/files/default/spiral-deploy.sh
+++ prometheus/files/default/spiral-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/srv/http/api/shared/bundle/ruby/1.9.1/bin:/srv/http/cms/shared/bundle/ruby/1.9.1/bin:/srv/http/web/shared/bundle/ruby/1.9.1/bin
+export PATH=$PATH:~/.composer/vendor/bin:/srv/http/api/shared/bundle/ruby/1.9.1/bin:/srv/http/cms/shared/bundle/ruby/1.9.1/bin:/srv/http/web/shared/bundle/ruby/1.9.1/bin
 
 export GEM_PATH=$GEM_PATH:/srv/http/api/shared/bundle/ruby/1.9.1:/srv/http/cms/shared/bundle/ruby/1.9.1:/srv/http/web/shared/bundle/ruby/1.9.1
