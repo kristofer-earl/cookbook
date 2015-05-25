9b402728f27c464d9cce59c3e632e19c9f6477a0
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-25 18:45:02 +0800

    rather move script into known location

--- zabbix/recipes/agent.rb
+++ zabbix/recipes/agent.rb
@@ -2,7 +2,7 @@ include_recipe 'zabbix::default'
 
 package 'zabbix-agent'
 
-template  '/usr/lib/zabbix/externalscripts/zbx_nginx_stats.py' do
+template  '/etc/zabbix/zbx_nginx_stats.py' do
   mode   '0755'
   owner  'root'
   group  'root'
