95bbc4d59c9b401fd6b877aebe2f4e8d91218604
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-25 11:40:57 +0800

    install zabbix-server with default mysql dep and frontend

--- zabbix/recipes/server.rb
+++ zabbix/recipes/server.rb
@@ -3,3 +3,17 @@ include_recipe 'zabbix::default'
 
 package 'zabbix-server-mysql'
 package 'zabbix-frontend-php'
+
+template '/etc/nginx/sites-available/zabbix-nginx.conf' do
+  source   'zabbix-nginx.conf.erb'
+  owner    'root'
+  group    'root'
+  mode     '0644'
+  action   :create
+  notifies :restart, "service[nginx]"
+end
+
+link '/etc/nginx/sites-enabled/zabbix-nginx.conf' do
+  to '/etc/nginx/sites-available/zabbix-nginx.conf'
+  notifies :restart, "service[nginx]"
+end
