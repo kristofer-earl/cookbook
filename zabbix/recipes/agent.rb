347c864869c2e3630d380337147a5958c069c5e6
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-05-26 18:43:11 +0800

    add install zabbix_agentd.conf

--- zabbix/recipes/agent.rb
+++ zabbix/recipes/agent.rb
@@ -6,8 +6,16 @@ template  '/etc/zabbix/zbx_nginx_stats.py' do
   mode   '0755'
   owner  'root'
   group  'root'
-  action :create
   backup false
   source 'zbx_nginx_stats.py.erb'
+  action :create
+end
+
+template '/etc/zabbix/zabbix_agentd.conf' do
+  mode   '0644'
+  owner  'root'
+  group  'root'
+  backup false
+  source 'zabbix_agentd.conf.erb'
   action :create
 end
