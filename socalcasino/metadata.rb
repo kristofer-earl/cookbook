c5e2a5ffe031f2a89bebb50b20e762b186a82dbe
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-06-13 17:51:33 +0800

    add spiral to deps

---
+++ socalcasino/metadata.rb
@@ -0,0 +1,18 @@
+name              'socialcasino'
+maintainer        'Social Casino stuff'
+maintainer_email  'devops@spiralwks.com'
+license           'Private'
+description       'Configures basic infrastruture resused within the contexts of many inhouse applications'
+version           '0.0.1'
+
+depends 'apt'
+depends 'supervisor'
+depends 'build-essential'
+depends 'hhvm3'
+depends 'mongodb'
+depends 'elasticsearch'
+depends 'graylog2'
+depends 'spiral'
+depends 'percona'
+
+supports 'ubuntu' 
