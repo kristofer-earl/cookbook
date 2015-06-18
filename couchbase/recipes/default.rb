7476dd338eeea928507e91f78d89f8cdedb51e1c
Marlon Yukim <marlon.yukim@spiralwks.com>
2015-06-18 15:57:10 +0800

    added password, memoryQuota, package name and package url to attributes

--- couchbase/recipes/default.rb
+++ couchbase/recipes/default.rb
@@ -22,3 +22,19 @@ service "couchbase-server" do
   supports :restart => true, :status => true
   action [:enable, :start]
 end
+
+http_request "initialize_cluster" do
+  url "http://127.0.0.1:8091/nodes/self/controller/settings"
+  action :post
+end
+
+execute "admin credentials" do
+  command "curl -d username=Administrator -d password=spiralworks -d port=8091 http://127.0.0.1:8091/settings/web"
+end
+
+#http_request "posting data" do
+#  action :post
+#  url "http://127.0.0.1:8091/settings/web"
+#  message ({:username => "Administrator", :password => "spiralworks", :port => "8091"}.to_json)
+#  headers({"AUTHORIZATION" => "Basic #{Base64.encode64("username:password")}","Content-Type" => "application/data"})
+#end
