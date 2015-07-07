4a57c298aa872f0ac9155366b52ecac15cd56e9c
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-07 14:00:00 +0800

    remove obsolete code

--- spiral/recipes/default.rb
+++ spiral/recipes/default.rb
@@ -10,15 +10,3 @@ execute 'git_url_default_to_https' do
   user 'deploy'
   environment ({ 'HOME' => '/home/deploy' })
 end
-
-# get RDS instance for MySQL connection
-#rds_instance_ip = Resolv.getaddress(node[:opsworks][:stack][:rds_instances].first[:address])
-#rds_name = 'rds1.localdomain'
-
-#bash "insert_line" do
-#  user "root"
-#  code <<-EOS
-#  echo "#{rds_instance_ip} #{rds_name}" >> /etc/hosts
-#  EOS
-#  not_if "grep -q #{rds_name} /etc/hosts"
-#end
