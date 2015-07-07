ff9ecf8c5ecfa1d81042d3d7865e835e2bcbd14a
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-07 14:04:24 +0800

    add graylog is enabled attribute with default set to true

--- spiral/attributes/default.rb
+++ spiral/attributes/default.rb
@@ -1,3 +1,4 @@
+default['spiral']['graylog']['enable'] = true
 default['spiral']['graylog']['host'] = 'graylog.spiralworks-cloud.com'
 default['spiral']['graylog']['syslog_udp']['port'] = '11514'
 default['spiral']['graylog']['gelf_udp']['port'] = '12197'
