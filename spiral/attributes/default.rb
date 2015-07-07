70b31b8e3f35499a4d053ba84621da2bcc4b7a51
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-07 13:59:19 +0800

    set graylog default server and input ports

---
+++ spiral/attributes/default.rb
@@ -0,0 +1,3 @@
+default['spiral']['graylog']['host'] = 'graylog.spiralworks-cloud.com'
+default['spiral']['graylog']['syslog_udp']['port'] = '11514'
+default['spiral']['graylog']['gelf_udp']['port'] = '12197'
