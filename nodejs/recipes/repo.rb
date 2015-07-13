c2c494ec5e3b899d485c3cbb60608ee133541269
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-13 17:01:20 +0800

    use ppa instead of nodesource, avoid keyserver usage

--- nodejs/recipes/repo.rb
+++ nodejs/recipes/repo.rb
@@ -4,13 +4,18 @@ when 'debian'
 
   package 'apt-transport-https'
 
-  apt_repository 'node.js' do
-    uri node['nodejs']['repo']
-    distribution node['lsb']['codename']
-    components ['main']
-    keyserver node['nodejs']['keyserver']
-    key node['nodejs']['key']
+  #apt_repository 'node.js' do
+  #  uri node['nodejs']['repo']
+  #  distribution node['lsb']['codename']
+  #  components ['main']
+  #  keyserver node['nodejs']['keyserver']
+  #  key node['nodejs']['key']
+  #end
+
+  execute 'node.js_ppa' do
+    command '/usr/bin/apt-add-repository -y ppa:chris-lea/node.js'
   end
+  
 when 'rhel'
   include_recipe 'yum-epel'
 end
