92a8c4a78c22b90129d930553ffeb68d12be2a43
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-22 13:29:42 +0800

    add statsd configuration defaults

--- spiral/attributes/default.rb
+++ spiral/attributes/default.rb
@@ -1,6 +1,8 @@
 default['spiral']['graylog']['enable'] = true
-default['spiral']['graylog']['host'] = 'graylog.spiralworks-cloud.com'
+default['spiral']['graylog']['host'] = 'graylog.local'
 default['spiral']['graylog']['syslog_udp']['port'] = '11514'
 default['spiral']['graylog']['gelf_udp']['port'] = '12197'
 default['spiral']['graylog']['nginx_err']['port'] = '12302'
 default['spiral']['graylog']['nginx_acc']['port'] = '12301'
+default['spiral']['statsd']['host'] = 'graphite.local'
+default['spiral']['statsd']['port'] = '8125'
