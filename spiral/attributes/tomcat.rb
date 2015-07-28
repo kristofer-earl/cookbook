d0c4f11b5f54b1aeeecde03ea196e9f78fae1b22
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-07-28 20:30:31 +0800

    upgrade tomcat 7.x to 7.0.63

--- spiral/attributes/tomcat.rb
+++ spiral/attributes/tomcat.rb
@@ -1,5 +1,5 @@
 default['spiral']['tomcat']['series'] = '7' 
-default['spiral']['tomcat']['version'] = '7.0.61'
+default['spiral']['tomcat']['version'] = '7.0.63'
 
 default['spiral']['tomcat']['url'] = "http://apache.mirrors.pair.com/tomcat/tomcat-#{node['spiral']['tomcat']['series']}/v#{node['spiral']['tomcat']['version']}/bin/apache-tomcat-#{node['spiral']['tomcat']['version']}.tar.gz"
 
