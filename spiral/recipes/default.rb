c3e4805d0440167f7d3c896594cf902ebfeb3e35
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2015-09-07 15:22:50 +0800

    use python based s3 transport instead

--- spiral/recipes/default.rb
+++ spiral/recipes/default.rb
@@ -6,13 +6,14 @@ package 'git'
 package 'nfs-common'
 package 'iptables-persistent'
 package 'awscli'
+package 'python-configobj'
 
-execute 'apt_s3_repo' do
-  command '/usr/bin/apt-add-repository ppa:leonard-ehrenfried/apt-transport-s3 && /usr/bin/apt-get update'
-  creates '/etc/apt/sources.list.d/leonard-ehrenfried-apt-transport-s3-trusty.list'
+remote_file '/usr/lib/apt/methods/s3' do
+  source 'https://raw.githubusercontent.com/kth5/apt-transport-s3/master/s3'
+  owner  'root'
+  group  'root'
+  mode   '755'
 end
-
-package 'apt-transport-s3'
 
 execute 'git_url_default_to_https' do
   command 'git config --global url."https://".insteadOf git://'
