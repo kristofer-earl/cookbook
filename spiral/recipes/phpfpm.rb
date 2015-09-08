e9f089d40bc1481ff16cdacd1a4d2f94531cb3fb
Alexander Baldeck <alexander.baldeck@mntechnology.com>
2015-09-08 12:37:38 +0800

    add path for geoip db and configure it

--- spiral/recipes/phpfpm.rb
+++ spiral/recipes/phpfpm.rb
@@ -31,6 +31,21 @@ directory '/srv/http/adm' do
   action :create
 end
 
+directory '/srv/http/adm/geoip' do
+  owner  'www-data'
+  group  'www-data'
+  mode   '0755'
+  action :create
+end
+
+cookbook_file '/etc/php5/mods-available/geoip.ini' do
+  source 'php5-geoip.ini'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+  action :create
+end
+
 directory '/srv/http/adm/phpfpm-cache-clear' do
   owner  'www-data'
   group  'www-data'
