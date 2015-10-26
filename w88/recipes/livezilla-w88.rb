8c6e565cc94621594b8254ae2faee7351175d87d
Kristofer Earl Saraspi <kristopher.saraspi@spiralwks.com>
2015-10-26 15:14:57 +0800

    add recipe for livezilla - w88

---
+++ w88/recipes/livezilla-w88.rb
@@ -0,0 +1,58 @@
+include_recipe 'percona::client'
+include_recipe 'spiral::phpfpm'
+
+directory '/srv/http' do
+  owner 'www-data'
+  group 'www-data'
+  mode  '0774'
+  action :create
+end
+
+directory '/srv/http/livezilla' do
+  owner 'www-data'
+  group 'www-data'
+  mode  '0774'
+  action :create
+end
+
+cookbook_file '/etc/nginx/sites-available/livezilla-en.conf' do
+  source   'lz-w88-en.conf'
+  owner    'root'
+  group    'root'
+  mode     '0644'
+  action   :create
+  notifies :restart, "service[nginx]"
+end
+
+cookbook_file '/etc/nginx/sites-available/livezilla-jp.conf' do
+  source   'lz-w88-jp.conf'
+  owner    'root'
+  group    'root'
+  mode     '0644'
+  action   :create
+  notifies :restart, "service[nginx]"
+end
+
+cookbook_file '/etc/nginx/sites-available/livezilla-id.conf' do
+  source   'lz-w88-id.conf'
+  owner    'root'
+  group    'root'
+  mode     '0644'
+  action   :create
+  notifies :restart, "service[nginx]"
+end
+
+link '/etc/nginx/sites-enabled/livezilla-en.conf' do
+  to '/etc/nginx/sites-available/livezilla-en.conf'
+  notifies :restart, "service[nginx]"
+end
+
+link '/etc/nginx/sites-enabled/livezilla-jp.conf' do
+  to '/etc/nginx/sites-available/livezilla-jp.conf'
+  notifies :restart, "service[nginx]"
+end
+
+link '/etc/nginx/sites-enabled/livezilla-id.conf' do
+  to '/etc/nginx/sites-available/livezilla-id.conf'
+  notifies :restart, "service[nginx]"
+end
